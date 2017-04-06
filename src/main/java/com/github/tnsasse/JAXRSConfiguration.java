b5751d27b1a314f9caa4ea1759cc8018acdab929
Tobias N. Sasse <tobi@byte23.de>
2017-04-06 18:55:50 +0200

    Added configuration facilities to restrict metrics exposed and path monitoring depth

--- src/main/java/com/github/tnsasse/JAXRSConfiguration.java
+++ src/main/java/com/github/tnsasse/JAXRSConfiguration.java
@@ -1,5 +1,13 @@
 package com.github.tnsasse;
 
+import com.github.tnsasse.boundary.PingResource;
+import com.github.tnsasse.jaxrsmonitor.boundary.MonitoringConfiguration;
+
+import java.util.logging.Logger;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
@@ -9,7 +17,25 @@ import javax.ws.rs.core.Application;
  *
  * @author airhacks.com
  */
+@Stateless
 @ApplicationPath("resources")
 public class JAXRSConfiguration extends Application {
+
+  @Inject
+  MonitoringConfiguration configuration;
+
+  @PostConstruct
+  public void setConfiguration() {
+    // These are all optional, too
+    configuration.setAppMetricsEnabled(true);
+    configuration.setSystemMetricsEnabled(true);
+    configuration.setResponseMetricsEnabled(true);
+    configuration.setMaxPathDepth(-1);
+
+    Logger logger = Logger.getLogger(PingResource.class.getName());
+    logger.info("App Metrics: " + configuration.isAppMetricsEnabled());
+    logger.info("System Metrics: " + configuration.isSystemMetricsEnabled());
+    logger.info("Response Metrics: " + configuration.isResponseMetricsEnabled());
+  }
 
 }
--- src/main/java/com/github/tnsasse/boundary/PingResource.java
+++ src/main/java/com/github/tnsasse/boundary/PingResource.java
@@ -1,18 +1,26 @@
 package com.github.tnsasse.boundary;
 
+import com.github.tnsasse.jaxrsmonitor.boundary.MonitoringConfiguration;
+
 import java.time.Instant;
+import java.util.logging.Logger;
+
+import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 /**
+ * Sample resource for generating responses to monitor
  *
  * @author tnsasse
  */
@@ -20,17 +28,30 @@ import javax.ws.rs.core.Response;
 @Path("/ping")
 @Produces(MediaType.APPLICATION_JSON)
 public class PingResource {
-    
-    @GET
-    public JsonObject getPing() {
-        JsonObjectBuilder builder = Json.createObjectBuilder();
-        builder.add("time", Instant.now().toString());
-        builder.add("message", "Hello Duke!");
-        return builder.build();
-    }
-    
-    @POST
-    public Response postPing(JsonObject payload) {
-        return Response.noContent().build();
-    }
+
+  @Inject
+  MonitoringConfiguration configuration; // optional
+
+
+  @GET
+  public JsonObject getPing() {
+    JsonObjectBuilder builder = Json.createObjectBuilder();
+    builder.add("time", Instant.now().toString());
+    builder.add("message", "Hello Duke!");
+    return builder.build();
+  }
+
+  @GET
+  @Path("/{message}")
+  public JsonObject getPingWithMessage(@PathParam("message") String message) {
+    JsonObjectBuilder builder = Json.createObjectBuilder();
+    builder.add("time", Instant.now().toString());
+    builder.add("message", message);
+    return builder.build();
+  }
+
+  @POST
+  public Response postPing(JsonObject payload) {
+    return Response.noContent().build();
+  }
 }
